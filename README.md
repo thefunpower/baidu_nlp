94f4941849e26377e6143177db7418b96c431658
sunkang <13127955213@qq.com>
2023-10-12 14:16:30 +0800

    get_baidu_nlp_address

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ baidu_nlp_app_id
 baidu_nlp_app_key
 baidu_nlp_app_secret
 ~~~
+
 # 使用
 
 [查看更多功能](https://ai.baidu.com/ai-doc/NLP/Mk6z52c9h#%E5%AE%89%E8%A3%85%E8%87%AA%E7%84%B6%E8%AF%AD%E8%A8%80%E5%A4%84%E7%90%86-php-sdk)
@@ -20,6 +21,12 @@ $client = get_baidu_nlp();
 $text = "上海市浦东新区纳贤路701号百度上海研发中心 F4A000 张三"; 
 // 调用地址识别接口
 $res = $client->address($text);
+~~~
+
+# 收货地址
+
+~~~
+get_baidu_nlp_address($address)
 ~~~
  
 
--- helper.php
+++ helper.php
@@ -1,15 +1,57 @@
 <?php 
+/**
+* 识别收货地址
+* province 省
+* city     市
+* region   区
+* street   详细地址
+* mobile   手机号
+* name     姓名
+*/
+function get_baidu_nlp_address($address,$is_ori = false){
+	$client = get_baidu_nlp();  
+	$res = $client->address($address); 
+	if($res['city']){
+		if($is_ori){
+			return $res;
+		}
+		$new = [];
+		$province = $res['province'];
+		$in = [
+			'北京市',
+			'天津市',
+			'上海市',
+			'重庆市',
+		];
+		if(in_array($province,$in)){
+			$province = str_replace("市","",$province);
+		}
+		$new['province'] = $province;
+		$new['city']   = $res['city'];
+		$new['region'] = $res['county'];
+		$new['street'] = $res['detail'];
+		$new['mobile'] = $res['phonenum']; 
+		$new['name'] = $res['person']; 
+		return $new;	
+	}else{
+		return;
+	}
+}
 
 /**
 * 初始化
 */
 function get_baidu_nlp($APP_ID = '',$API_KEY = '',$SECRET_KEY = ''){
+	static $baidu_nlp_init;
 	$APP_ID     = $APP_ID?:get_config("baidu_nlp_app_id");
 	$API_KEY    = $API_KEY?:get_config("baidu_nlp_app_key");
 	$SECRET_KEY = $SECRET_KEY?:get_config("baidu_nlp_app_secret");
 	if(!$APP_ID || !$API_KEY || !$SECRET_KEY){
 		die("请正确配置 baidu_nlp_app_id  baidu_nlp_app_key  baidu_nlp_app_secret");
 	}
-	$client = new AipNlp(trim($APP_ID), trim($API_KEY), trim($SECRET_KEY)); 
-	return $client;
+	$key = $APP_ID.$API_KEY;
+	if($baidu_nlp_init[$key]){
+		return $baidu_nlp_init[$key];
+	}
+	return $baidu_nlp_init[$key] = new AipNlp(trim($APP_ID), trim($API_KEY), trim($SECRET_KEY));  
 }
