6e08549161325b03fe0d27085135d67dd8570d4a
sunkang <13127955213@qq.com>
2023-10-12 13:48:45 +0800

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,9 +1,13 @@
 <?php 
 
+
 function get_baidu_nlp($APP_ID = '',$API_KEY = '',$SECRET_KEY = ''){
 	$APP_ID     = $APP_ID?:get_config("baidu_nlp_app_id");
 	$API_KEY    = $APP_ID?:get_config("baidu_nlp_app_key");
 	$SECRET_KEY = $APP_ID?:get_config("baidu_nlp_app_secret");
-	$client = new AipNlp($APP_ID, $API_KEY, $SECRET_KEY); 
+	if(!$APP_ID || !$API_KEY || !$SECRET_KEY){
+		die("请正确配置 baidu_nlp_app_id  baidu_nlp_app_key  baidu_nlp_app_secret");
+	}
+	$client = new AipNlp(trim($APP_ID), trim($API_KEY), trim($SECRET_KEY)); 
 	return $client;
 }
